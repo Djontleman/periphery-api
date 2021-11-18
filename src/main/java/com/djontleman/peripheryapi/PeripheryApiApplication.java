e3c95b37cc1ede857859ba1e43f052573553e51e
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-18 22:31:51 +0000

    Initiate basic spring boot application

---
+++ src/main/java/com/djontleman/peripheryapi/PeripheryApiApplication.java
@@ -0,0 +1,13 @@
+package com.djontleman.peripheryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeripheryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeripheryApiApplication.class, args);
+	}
+
+}
