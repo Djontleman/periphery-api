9afed59bbb98464ed23b866cc0cf083faa7b5008
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-26 23:57:15 +0000

    chore: add V7 to db/migration and add to sql data

---
+++ src/main/resources/db/migration/V7__Add_More_Songs_Data.sql
@@ -0,0 +1,77 @@
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('A Black Minute', '0:04:16', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('MK Ultra', '0:02:50', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Heavy Heart', '0:04:22', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('The Event', '0:01:45', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('The Scourge', '0:05:36', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Alpha', '0:05:31', 4, true, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('22 Faces', '0:03:52', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Rainbow Gravity', '0:04:39', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Four Lights', '0:02:18', 4, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Psychosphere', '0:06:16', 4, false, null);
+
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Reprise', '0:01:25', 5, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('The Bad Thing', '0:05:54', 5, true, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Priestess', '0:05:04', 5, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Graveless', '0:03:56', 5, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Hell Below', '0:03:43', 5, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Omega', '0:11:44', 5, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Stranger Things', '0:07:35', 5, false, null);
+
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('The Price Is Wrong', '0:03:57', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Motormouth', '0:04:50', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Marigold', '0:07:20', 6, true, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('The Way The News Goes...', '0:05:04', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Remain Indoors', '0:06:10', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Habitual Line-Stepper', '0:06:52', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Flatline', '0:05:51', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Absolomb', '0:07:44', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Catch Fire', '0:03:54', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Prayer Position', '0:04:37', 6, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Lune', '0:07:47', 6, false, null);
+
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Reptile', '0:16:44', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Blood Eagle', '0:05:58', 7, true, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Chvrch Bvrner', '0:03:41', 7, true, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Garden In The Bones', '0:05:57', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('It''s Only Smiles', '0:05:33', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Follow Your Ghost', '0:05:24', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Crush', '0:06:49', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Sentient Glow', '0:04:28', 7, false, null);
+INSERT INTO songs (song_name, song_duration, album_id, has_music_video, tuning_id)
+VALUES ('Satellites', '0:09:25', 7, false, null);
