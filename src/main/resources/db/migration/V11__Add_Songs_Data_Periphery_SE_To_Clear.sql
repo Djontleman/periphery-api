405c0875e026a2fe3d3cb7fd8a2d87ec70c3f98f
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-13 19:21:41 +0000

    chore: add V11, songs data from periphery SE to Clear

---
+++ src/main/resources/db/migration/V11__Add_Songs_Data_Periphery_SE_To_Clear.sql
@@ -0,0 +1,60 @@
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Insomnia (Instrumental)', '0:04:49', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('The Walk (Instrumental)', '0:05:05', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Letter Experiment (Instrumental)', '0:06:50', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Jetpacks Was Yes! (Instrumental)', '0:03:56', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Light (Instrumental)', '0:05:50', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('All New Materials (Instrumental)', '0:05:20', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Buttersnips (Instrumental)', '0:05:53', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Icarus Lives! (Instrumental)', '0:03:09', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Totla Mad (Instrumental)', '0:03:59', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Ow My Feelings (Instrumental)', '0:06:06', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Zyglrox (Instrumental)', '0:05:05', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Racecar (Instrumental)', '0:14:55', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Passenger', '0:03:35', false, null);
+
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('New Groove (Instrumental)', '0:01:44', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Frak The Gods (Instrumental)', '0:03:31', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Jetpacks Was Yes! (V2.0)', '0:04:24', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Icarus Lives! (EP Version)', '0:03:13', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Icarus Lives! (Bulbous Remix)', '0:03:46', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Icarus Lives! (Zedd Remix)', '0:04:55', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Icarus Lives! (Petey G Remix)', '0:03:46', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Captain On (EP Version)', '0:03:11', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Eureka!', '0:05:16', false, null);
+
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Overture', '0:02:11', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('The Summer Jam', '0:04:17', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Feed The Ground', '0:04:37', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Zero', '0:05:30', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('The Parade Of Ashes', '0:05:12', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Extraneous', '0:03:20', false, null);
+INSERT INTO songs (song_name, song_duration, has_music_video, tuning_id)
+VALUES ('Pale Aura', '0:04:39', false, null);
