084527ff51e4f69cc02f6acf35981feaafbc55d6
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-13 18:56:17 +0000

    chore: add V10 albums_songs data for Periphery II to Periphery IV

---
+++ src/main/resources/db/migration/V10__Add_Albums_Songs_Data_For_Periphery_II_To_Periphery_IV.sql
@@ -0,0 +1,106 @@
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 13);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 14);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 15);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 16);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 17);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 18);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 19);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 20);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 21);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 22);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 23);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 24);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 25);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (3, 26);
+
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 27);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 28);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 29);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 30);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 31);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 32);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 33);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 34);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 35);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (4, 36);
+
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 37);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 38);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 39);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 40);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 41);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 42);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (5, 43);
+
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 44);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 45);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 46);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 47);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 48);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 49);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 50);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 51);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 52);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 53);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (6, 54);
+
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 55);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 56);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 57);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 58);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 59);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 60);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 61);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 62);
+INSERT INTO albums_songs (album_id, song_id)
+VALUES (7, 63);
