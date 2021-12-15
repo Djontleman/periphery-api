6ebf4e85eb8e6fe06ba996348747a74dd0f40003
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-12-15 09:59:59 +0000

    refactor: replace code in Album and Song with lombok annotations

--- src/main/java/com/djontleman/album/Album.java
+++ src/main/java/com/djontleman/album/Album.java
@@ -2,12 +2,18 @@ package com.djontleman.album;
 
 import com.djontleman.song.Song;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Album {
     private int id;
     private String name;
@@ -17,85 +23,4 @@ public class Album {
     @JsonIgnoreProperties("albumsOn")
     private List<Song> songList;
 
-    public Album(int id, String name, AlbumType type, LocalDate releaseDate, LocalTime duration, List<Song> songList) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.songList = songList;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public AlbumType getType() {
-        return type;
-    }
-
-    public void setType(AlbumType type) {
-        this.type = type;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public LocalTime getDuration() {
-        return duration;
-    }
-
-    public void setDuration(LocalTime duration) {
-        this.duration = duration;
-    }
-
-    public List<Song> getSongList() {
-        return songList;
-    }
-
-    public void setSongList(List<Song> songList) {
-        this.songList = songList;
-    }
-
-    @Override
-    public String toString() {
-        return "Album{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", type=" + type +
-                ", releaseDate=" + releaseDate +
-                ", duration=" + duration +
-                ", songList=" + songList +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Album album = (Album) o;
-        return id == album.id && Objects.equals(name, album.name) && type == album.type && Objects.equals(releaseDate, album.releaseDate) && Objects.equals(duration, album.duration) && Objects.equals(songList, album.songList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, type, releaseDate, duration, songList);
-    }
 }
--- src/main/java/com/djontleman/song/Song.java
+++ src/main/java/com/djontleman/song/Song.java
@@ -2,11 +2,17 @@ package com.djontleman.song;
 
 import com.djontleman.album.Album;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Song {
    private int id;
    private String name;
@@ -16,85 +22,4 @@ public class Song {
    @JsonIgnoreProperties({"songList", "duration"})
    private List<Album> albumsOn;
 
-    public Song(int id, String name, LocalTime duration, boolean hasMusicVideo, Integer tuningId, List<Album> albumsOn) {
-        this.id = id;
-        this.name = name;
-        this.duration = duration;
-        this.hasMusicVideo = hasMusicVideo;
-        this.tuningId = tuningId;
-        this.albumsOn = albumsOn;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalTime getDuration() {
-        return duration;
-    }
-
-    public void setDuration(LocalTime duration) {
-        this.duration = duration;
-    }
-
-    public boolean isHasMusicVideo() {
-        return hasMusicVideo;
-    }
-
-    public void setHasMusicVideo(boolean hasMusicVideo) {
-        this.hasMusicVideo = hasMusicVideo;
-    }
-
-    public Integer getTuningId() {
-        return tuningId;
-    }
-
-    public void setTuningId(Integer tuningId) {
-        this.tuningId = tuningId;
-    }
-
-    public List<Album> getAlbumsOn() {
-        return albumsOn;
-    }
-
-    public void setAlbumsOn(List<Album> albumsOn) {
-        this.albumsOn = albumsOn;
-    }
-
-    @Override
-    public String toString() {
-        return "Song{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", duration=" + duration +
-                ", hasMusicVideo=" + hasMusicVideo +
-                ", tuningId=" + tuningId +
-                ", albumsOn=" + albumsOn +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Song song = (Song) o;
-        return id == song.id && hasMusicVideo == song.hasMusicVideo && Objects.equals(name, song.name) && Objects.equals(duration, song.duration) && Objects.equals(tuningId, song.tuningId) && Objects.equals(albumsOn, song.albumsOn);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, duration, hasMusicVideo, tuningId, albumsOn);
-    }
 }
